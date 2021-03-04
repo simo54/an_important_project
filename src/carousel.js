c00e1e4c1671314e714786a519728daec0b98e49
simone <sorzesimone1@gmail.com>
2021-03-04 22:52:05 +0100

    change carousel and importend photos test

--- src/carousel.js
+++ src/carousel.js
@@ -1,5 +1,33 @@
 import _ from "lodash";
-
-import Splide from "@splidejs/splide";
-
-new Splide(".splide").mount();
+// import Swiper JS
+import Swiper from "swiper/core";
+import "swiper/swiper-bundle.css";
+// Swiper Configuration
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1.5,
+  spaceBetween: 10,
+  centeredSlides: true,
+  freeMode: true,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 1.5,
+    },
+  },
+});
